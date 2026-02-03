cd8ff8a5eda94581fc06e1256c782487b309db37
unknown <choprasayansh@gmail.com>
2026-02-03 15:51:27 +0530

    implemented forget password API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -3,6 +3,8 @@ const User=require("../models/user.js");
 const profileRouter=express.Router()
 const {userAuth}=require("../middlewares/auth.js");
 const {validateEditProfileData}=require("../utils/validation.js");
+const bcrypt=require("bcrypt");
+const validator=require("validator");
 
 
 profileRouter.get("/profile/view",userAuth,async(req,res)=>{
@@ -32,6 +34,32 @@ profileRouter.patch("/profile/edit",userAuth,async(req,res)=>{
   
   }catch(err){
     res.status(400).send("Error occured while editing the profile  : "+err.message);
+  }
+})
+
+profileRouter.patch("/profile/forgetpassword",async(req,res)=>{
+  try{
+
+     const {emailId}=req.body;
+
+     const {newPassword}=req.body;
+     const user=await User.findOne({emailId});
+
+     if(!user){
+      return res.status(400).send({success:false,message:"user does not exists"})
+     }
+     if(!validator.isStrongPassword(newPassword)){
+       throw new Error("password is not strong enough");
+     }
+     
+     const hashedPassword=await bcrypt.hash(newPassword,10);
+    
+     await User.findByIdAndUpdate(user._id,{password:hashedPassword});
+
+     res.status(200).send({success:true,message:"password changed successfully"});
+
+  }catch(err){
+    res.status(400).json({success:false,message:err.message})
   }
 })
 
