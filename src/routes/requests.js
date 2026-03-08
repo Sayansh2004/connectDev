5c3111cc403cd2fc5a42fddb84aa8f69d2d8ffe3
unknown <choprasayansh@gmail.com>
2026-03-08 23:53:29 +0530

    fixed bugs regarding sockets

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -17,7 +17,7 @@ requestRouter.post("/request/send/:status/:touserId",userAuth,async(req,res)=>{
 
 
        const allowedStatus=["ignored","interested"];
-
+          
        if(!allowedStatus.includes(status)){
         return res.status(400).json({success:false,message:"Invalid status type : "+status});
        }
